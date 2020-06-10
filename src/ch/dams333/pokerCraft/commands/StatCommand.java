93694831590acd36118cedd744127eab4b42974c
Damien <damien.hubleur@gmail.com>
2020-06-10 20:54:54 +0200

    better visualizing stats

--- src/ch/dams333/pokerCraft/commands/StatCommand.java
+++ src/ch/dams333/pokerCraft/commands/StatCommand.java
@@ -23,16 +23,16 @@ public class StatCommand implements CommandExecutor {
                 if(main.statistiquesManager.pseudoHasStat(pseudo) && main.statistiquesManager.getPseudoStat(pseudo).getRounds() > 0){
                     Statistique stat = main.statistiquesManager.getPseudoStat(pseudo);
                     p.sendMessage(ChatColor.DARK_GREEN + "======= " + pseudo + " ... =======");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "a prit part à " + stat.getGames() + " parties" + " pour un total de " + stat.getCompleteRound() + " rounds");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "a prit part à " + ChatColor.BOLD + stat.getGames() + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + " parties" + " pour un total de " + ChatColor.BOLD + stat.getCompleteRound() + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + " rounds");
                     p.sendMessage(" ");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "se couche dans " + (Math.round((stat.getCouches() * 100) / stat.getRounds())) + "% du temps");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "check dans " + (Math.round((stat.getCheck() * 100) / stat.getRounds())) + "% du temps");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "suit dans " + (Math.round((stat.getFollow() * 100) / stat.getRounds())) + "% du temps");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "relance dans " + (Math.round((stat.getRelance() * 100) / stat.getRounds())) + "% du temps");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "fait tapis dans " + (Math.round((stat.getTapis() * 100) / stat.getRounds())) + "% du temps");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "se couche " + ChatColor.BOLD + (Math.round((stat.getCouches() * 100) / stat.getRounds())) + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + "% du temps");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "check " + ChatColor.BOLD + (Math.round((stat.getCheck() * 100) / stat.getRounds())) + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + "% du temps");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "suit " + ChatColor.BOLD + (Math.round((stat.getFollow() * 100) / stat.getRounds())) + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + "% du temps");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "relance " + ChatColor.BOLD + (Math.round((stat.getRelance() * 100) / stat.getRounds())) + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + "% du temps");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "fait tapis " + ChatColor.BOLD + (Math.round((stat.getTapis() * 100) / stat.getRounds())) + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + "% du temps");
                     p.sendMessage(" ");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "à gagner " + stat.getWinsRound() + " rounds");
-                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "à gagner " + stat.getWinsGame() + " games");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "à gagner " + ChatColor.BOLD + stat.getWinsRound() + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + " rounds");
+                    p.sendMessage(ChatColor.GRAY + "... " + ChatColor.LIGHT_PURPLE + "à gagner " + ChatColor.BOLD + stat.getWinsGame() + ChatColor.RESET + "" + ChatColor.LIGHT_PURPLE + " games");
                     return true;
                 }
                 p.sendMessage(ChatColor.RED + "Ce joueur n'a pas de statistique");
